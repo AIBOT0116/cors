4eda9eb44ec10d77eaf5e3c31fb541c8cf59029c
AIBOT0116 <pijushandco0@gmail.com>
2025-06-07 00:55:41 +0530

    1255

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@ const { parse } = require('url');
 
 module.exports = async (req, res) => {
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.sonyliv.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader("Access-Control-Allow-Headers", 'Content-Type, Authorization');
 
